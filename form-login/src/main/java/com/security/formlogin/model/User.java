8af6eba3e84bb8d9eec30ad0ebc6509471da309b
EunsilSon <eunsil1023@gmail.com>
2024-06-07 22:18:56 +0900

    Feat : User 엔티티, 레포지토리

---
+++ form-login/src/main/java/com/security/formlogin/model/User.java
@@ -0,0 +1,28 @@
+package com.security.formlogin.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String role; // ROLE_USER, ROLE_ADMIN
+
+}
---
+++ form-login/src/main/java/com/security/formlogin/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.security.formlogin.repository;
+
+import com.security.formlogin.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByUsername(String username);
+
+    User findByUsername(String username);
+
+}
